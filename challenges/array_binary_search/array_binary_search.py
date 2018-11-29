0b3b92e224b70cae1b3fc54d71f961106924ce79
Scott Currie <vitriolik@gmail.com>
2018-11-28 20:58:26 -0800

    Corrected algorithm. Removed extraneous code.

--- challenges/array_binary_search/array_binary_search.py
+++ challenges/array_binary_search/array_binary_search.py
@@ -1,23 +1,23 @@
 def binary_search(search_list, search_key):
+    """Find the index of a value of a key in a list usings a
+    binary search algorithg. Returns the index of the value if
+    found. Otherwise, returns -1.
+    """
     left_idx, right_idx = 0, len(search_list) - 1
     # while True:
-    for _ in range(10):
-        if right_idx - left_idx < 1:
-            return -1
-        mid_idx = (right_idx - left_idx) // 2
-        # search_key is at mid_idx
-        if search_key == search_list[mid_idx]:
-            return mid_idx
-        # search_key is left of mid_idx
+    while left_idx <= right_idx:
+        mid_idx = (right_idx + left_idx) // 2
+        # search_key is left of middle value
         if search_key < search_list[mid_idx]:
-            right_idx = mid_idx
+            right_idx = mid_idx - 1
+        # search key is right of middle value
         elif search_key > search_list[mid_idx]:
-            left_idx = mid_idx
-        # Subrange is empty
-
-        print('search_key=', search_key, 'left_idx=', left_idx, 'mid_idx=', mid_idx,
-              'search_list[mid_idx]=', search_list[mid_idx], 'right_idx=', right_idx)
+            left_idx = mid_idx + 1
+        else:
+            return mid_idx
+    # If we get here, the value was not found.
+    return -1
 
 
 if __name__ == '__main__':
-    pass
+    binary_search([1, 2, 3], 4) == -1
