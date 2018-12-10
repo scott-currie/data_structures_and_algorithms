385c6d26f9f5832049b3a1794c79f1d1ded33852
Scott Currie <vitriolik@gmail.com>
2018-12-09 22:34:10 -0800

    Fixed ll_merge implementation

--- challenges/ll_merge/ll_merge.py
+++ challenges/ll_merge/ll_merge.py
@@ -3,28 +3,52 @@ from .node import Node
 
 
 def ll_merge(ll1, ll2):
-    print('ll1', ll1)
-    print('ll2', ll2)
     merged = LinkedList()
-    merged.head = ll1.head
-    merged._size = ll1._size
-    cur_merged = ll1.head
-    ll1.head = ll1.head._next
-    cur_merged._next = ll2.head
-    ll2.head = ll2.head._next
-    cur_merged = cur_merged._next
-    while ll1.head._next or ll2.head._next:
-        cur_merged._next = ll1.head
+    c1 = ll1.head
+    c2 = ll2.head
 
-        ll1.head = ll1.head._next
-        cur_merged = cur_merged._next
-        cur_merged._next = ll2.head
-        ll2.head = ll2.head._next
-        cur_merged = cur_merged._next
+    while c1 or c2:
+        print('ll1=', ll1)
+        if c1:
+            # If merged head is None, it's our first pass
+            if merged.head is not None:
+                # Next of mc is ll1 head
+                mc._next = c1
+                # Move mc to its next
+                mc = mc._next
+                # Move c1 to its next
+                c1 = c1._next
+                merged._size += 1
+            # Not our first pass
+            else:
+                # If this list has reached the end, c1 is None
+                if c1 is not None:
+                    # Merged head is ll1 head
+                    merged.head = c1
+                    # mc is merged head
+                    mc = merged.head
+                    # Move current of ll1 to its next
+                    c1 = c1._next
+                    merged._size += 1
+        if c2:
+            if merged.head is not None:
+                # Next of mc is ll2 head
+                mc._next = c2
+                # Move current of ll2 to its next
+                c2 = c2._next
+                # Move mc to its next
+                mc = mc._next
+                mc._next = None
+                merged._size += 1
+            else:
+                merged.head = c2
+                mc = merged.head
+                c2 = c2._next
+                mc._next = None
+                merged._size += 1
+        print('merged=', merged)
 
-        print('ll1', ll1)
-        print('ll2', ll2)
-        print('merged =', merged)
+
 
     print(merged)
     return merged
