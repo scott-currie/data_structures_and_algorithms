98006981e0c637a66d01c6ea806ca6f965e38f92
Scott Currie <vitriolik@gmail.com>
2018-12-18 16:20:26 -0800

    Added some assertions

--- challenges/breadth_first_traversal/test_breadth_first_traversal.py
+++ challenges/breadth_first_traversal/test_breadth_first_traversal.py
@@ -1,6 +1,5 @@
 from .breadth_first_traversal import breadth_first_traversal
 from data_structures_and_algorithms.data_structures.binary_search_tree.bst import BST
-import pytest
 
 
 def test_imports():
@@ -9,15 +8,25 @@ def test_imports():
 
 
 def test_bft(capsys):
-    # bst = BST([5, 3, 2, 11, 27, 25])
-    # # bst.in_order(bst.root)
-    # breadth_first_traversal(bst)
-    # captured = capsys.readouterr()
-    # expected = '5\n3\n11\n2\n27\n25\n'
-    # assert captured.out == expected
+    """Test breadth_first_traversal function with some test trees
+    and compare captured stdout with expected values."""
     bst = BST([2, 1, 3])
-    # bst.in_order(bst.root)
     breadth_first_traversal(bst)
     captured = capsys.readouterr()
     expected = '2\n1\n3\n'
+    assert captured.out == expected
+    bst = BST([3, 2, 1])
+    breadth_first_traversal(bst)
+    captured = capsys.readouterr()
+    expected = '3\n2\n1\n'
+    assert captured.out == expected
+    bst = BST([1, 2, 3])
+    breadth_first_traversal(bst)
+    captured = capsys.readouterr()
+    expected = '1\n2\n3\n'
+    assert captured.out == expected
+    bst = BST([2, 1, 3, 4])
+    breadth_first_traversal(bst)
+    captured = capsys.readouterr()
+    expected = '2\n1\n3\n4\n'
     assert captured.out == expected
