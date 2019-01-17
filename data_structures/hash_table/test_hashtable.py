2e1f0fc5c3cf333a6c9b6629bd64d46bd6ebbb90
Scott Currie <vitriolik@gmail.com>
2019-01-17 09:19:48 -0800

    Updated tests to reflect changes to HashTable class.

--- data_structures/hash_table/test_hashtable.py
+++ data_structures/hash_table/test_hashtable.py
@@ -9,33 +9,34 @@ def test_import():
 def test_add_new_key():
     """Test adding a valid entry that doesn't already exist."""
     ht = HashTable()
-    assert ht.add(0, 1) == True
-    assert ht.add('0', 1) == True
-    assert ht.add((0, 0), 1) == True
+    assert ht.set(0, 1) == True
+    assert ht.set('0', 1) == True
+    assert ht.set((0, 0), 1) == True
 
 def test_add_existing_key():
     """Test adding a valid entry that already exists."""
     ht = HashTable()
-    ht.add(0,1)
-    with pytest.raises(KeyError):
-        ht.add(0, 1)
+    ht.set(0,1)
+    assert ht.get(0) == 1
+    ht.set(0, 3)
+    assert ht.get(0) == 3
 
 def test_add_invalid_type():
     """Test adding an item with an invalid key."""
     ht = HashTable()
     with pytest.raises(TypeError):
-        ht.add([1, 2, 3], 9)
+        ht.set([1, 2, 3], 9)
 
 def test_get_valid_key():
     """Test existing key returns expected value."""
     ht = HashTable()
-    ht.add(0, 1)
+    ht.set(0, 1)
     assert ht.get(0) == 1
 
 def test_get_invalid_key():
     """Test that getting a nonexistent key returns None."""
     ht = HashTable()
-    ht.add(0, 1)
+    ht.set(0, 1)
     assert ht.get(1) is None
 
 def test_get_idx_collision():
@@ -46,16 +47,16 @@ def test_get_idx_collision():
 def test_get_key_collision():
     """Test that two keys that hash to the same value are retrieved successfully."""
     ht = HashTable()
-    ht.add(4, 'four')
-    ht.add(36, 'thirty-six')
+    ht.set(4, 'four')
+    ht.set(36, 'thirty-six')
     assert ht.get(4) == 'four'
     assert ht.get(36) == 'thirty-six'
 
 def test_remove_key_collision():
     """Test that two keys that hash to the same value are retrieved successfully."""
     ht = HashTable()
-    ht.add(4, 'four')
-    ht.add(36, 'thirty-six')
+    ht.set(4, 'four')
+    ht.set(36, 'thirty-six')
     assert ht.get(4) == 'four'
     assert ht.get(36) == 'thirty-six'
     ht.remove(36)
